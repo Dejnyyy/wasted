f9996304d7b5c5aa9adc2dffa970e48414c3c02c
Dejnyyy <danslaby@email.cz>
2025-04-04 22:53:02 -0400

    tak tak

--- src/pages/components/ProductCard/index.tsx
+++ src/pages/components/ProductCard/index.tsx
@@ -9,10 +9,10 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ product, onClick }) => {
   return (
     <div
-      className="bg-white text-black p-6 rounded-lg flex flex-col items-center cursor-pointer"
+      className="bg-white text-black p-6 rounded-xl flex flex-col items-center cursor-pointer"
       onClick={onClick}
     >
-      <Image src={product.image} alt={product.name} width={200} height={200} />
+      <Image src={product.image} alt={product.name} width={200} height={200} className='rounded-xl hover:scale-110 duration-200  transition-all ' />
       <p className="text-sm text-pink-600 mt-4">COMING SOON</p>
       <h3 className="font-bold mt-2">{product.name}</h3>
       
